119cc67fa8072503741e6a92a7eacf2515f7512b
plainoldprogrammer <plainoldprogrammer@gmail.com>
2019-10-30 14:39:52 -0500

    defining the snippet class

--- snippet.cpp
+++ snippet.cpp
@@ -5,3 +5,23 @@ Snippet::Snippet()
 {
 	qDebug() << "New snippet created";
 }
+
+void Snippet::setTitle(QString title)
+{
+	this->title = title;
+}
+
+QString Snippet::getTitle()
+{
+	return this->title;
+}
+
+void Snippet::setContent(QString content)
+{
+	this->content = content;
+}
+
+QString Snippet::getContent()
+{
+	return this->content;
+}
--- snippet.h
+++ snippet.h
@@ -1,10 +1,21 @@
 #ifndef SNIPPET_H
 #define SNIPPET_H
 
+#include <QString>
+
 class Snippet
 {
 	public:
 		Snippet();
+		
+		void setTitle(QString);
+		QString getTitle();
+		
+		void setContent(QString);
+		QString getContent();
+	private:
+		QString title;
+		QString content;
 };
 
 #endif
