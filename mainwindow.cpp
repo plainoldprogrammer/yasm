4846f933fff7de7d6b1922a19cd400c6d7aaaa05
plainoldprogrammer <plainoldprogrammer@gmail.com>
2019-10-30 14:11:58 -0500

    setting the ui attributes for first time launch

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,13 @@ MainWindow::MainWindow(QWidget *parent)
 , ui(new Ui::MainWindow)
 {
 	ui->setupUi(this);
+	
+	ui->pushButtonNewSnippet->setText("New Snippet");
+	this->setWindowTitle("YASM (Alpha)");
+	
+	ui->listViewSnippets->setEnabled(false);
+	ui->lineEditSnippetTitle->setEnabled(false);
+	ui->textEditSnippetContent->setEnabled(false);
 }
 
 MainWindow::~MainWindow()
