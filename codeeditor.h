240354ed9eaaeb940b3bf5cbf4992a845f56ef42
plainoldprogrammer <plainoldprogrammer@gmail.com>
2020-05-23 23:00:39 -0500

    creating a code editor widget to show line numbers and highlight current line

---
+++ codeeditor.h
@@ -0,0 +1,31 @@
+#ifndef CODEEDITOR_H
+#define CODEEDITOR_H
+
+#include <QWidget>
+#include <QPlainTextEdit>
+#include <QPainter>
+#include <QTextBlock>
+
+class CodeEditor : public QPlainTextEdit
+{
+    Q_OBJECT
+
+public:
+    CodeEditor(QWidget *parent = nullptr);
+
+    void lineNumberAreaPaintEvent(QPaintEvent *event);
+    int lineNumberAreaWidth();
+
+protected:
+    void resizeEvent(QResizeEvent *event) override;
+
+private slots:
+    void updateLineNumberAreaWidth(int newBlockCount);
+    void highlightCurrentLine();
+    void updateLineNumberArea(const QRect &rect, int dy);
+
+private:
+    QWidget *lineNumberArea;
+};
+
+#endif // CODEEDITOR_H
--- yasm.pro
+++ yasm.pro
@@ -16,6 +16,7 @@ DEFINES += QT_DEPRECATED_WARNINGS
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
+    codeeditor.cpp \
     main.cpp \
     mainwindow.cpp \
     optionsdialog.cpp \
@@ -23,6 +24,8 @@ SOURCES += \
     snippet.cpp
 
 HEADERS += \
+    codeeditor.h \
+    linenumberarea.h \
     mainwindow.h \
     optionsdialog.h \
     runguard.h \
